db97a519db29e03739b437dead6bee6d67b3e53c
jose <jlzsisco@gmail.com>
2015-03-14 00:09:57 -0600

    actualizacion 2

---
+++ Practicas/paginaITC/js/funciones.js
@@ -0,0 +1,9 @@
+			$(document).ready(function(){		
+
+					function googleTranslateElementInit() {
+  					new google.translate.TranslateElement({pageLanguage: 'es', includedLanguages: 'ca,de,en,es,fr,ja,pt,zh-CN', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, autoDisplay: false}, 'google_translate_element');
+					}
+					
+
+});
+
