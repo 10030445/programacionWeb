dc1d8a6363792f74ef8d060b0865729f20f43f6a
jose <jlzsisco@gmail.com>
2015-04-29 13:05:36 -0500

    php2

--- Practicas/UNIDAD 2 Y 4/www/sitioweb/hola.php
+++ Practicas/UNIDAD 2 Y 4/www/sitioweb/hola.php
@@ -9,15 +9,23 @@ $objcon = new conexion();
 $con = $objcon->conectar();
 
 $usr = $_POST['txtnombre'];
-$usr = $_POST['txtpass'];
+$pwd = $_POST['txtpass'];
 
-$qry = "SELEST * FROM usuarios WHERE usr='".$usr."' AND pwdusr = '".md5($pwd). ."'";
+$qry = "SELECT * FROM usuarios WHERE usr='".$usr."' AND pwdusr = '".md5($pwd)."'";
 $con_usr = mysql_query($qry,$con);
 $num_res = mysql_num_rows($con_usr);
 if( $num_res == 1)
 {
-
-		$_SESSION['nomusr'] =  "Rubensin :(";
+		
+		$dat_usr = mysql_fetch_array($con_usr); //arreglo asociativo
+		$_SESSION['nomusr'] =  $dat_usr['nomusr'];
 		header('Location:./carpeta/pagina3.php');
 }
+
+		else
+
+{
+		header('Location:index.php');
+}
+
 ?>
--- Practicas/UNIDAD 2 Y 4/www/sitioweb/index.php
+++ Practicas/UNIDAD 2 Y 4/www/sitioweb/index.php
@@ -1,14 +1,21 @@
 <?php
+session_start()
 echo 'Hello World';
 echo "<HMTL>";
 	echo "<HEAD><title>	Mi primer página PHP</title></HEAD>";
 	echo "<BODY>";		
 	echo "<FORM name='frmdatos' method='post' action='hola.php?name=Luis&apepat=Silva' target='_self'>";
-	echo "<input type='text' name='txtnombre' size='30'>";
+	echo "Usuario<input type='text' name='txtnombre' size='30'><br>";
+    echo "Password<input type='password' name='txtpass' size='30'><br>";
 	echo "<input type='submit' value='Enviar'>";
 	echo "<a href='hola.php?name=Luis&apepat=Silva'>Hola</a>";
 	echo "</FORM>";
 
 	echo "</BODY";
-echo "</HTML>";		
+echo "</HTML>";	
+
+  echo  $_SESSION ['nomusr'] = "Luis:)";
+  echo $_SESSION ['apepat'] = "Silva";
+  echo $_SESSION ['correo'] = "jlzsisco@gmail.colm";
+
 ?>
